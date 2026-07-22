09404225937ce442392f443aa10683523b7968c8
Soham-2K15 <soham2k15@gmail.com>
2026-07-22 14:48:04 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -4,20 +4,15 @@ import java.awt.*;
 public class DisplayImage {
 
     public static void main(String[] args) {
-        // Create the frame
         JFrame frame = new JFrame("Education Image");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(650, 500);
 
-        // Load the image
         ImageIcon image = new ImageIcon("C:\Users\Dell\Desktop\SOHAM\education.png");
-        // Create a label to hold the image
         JLabel label = new JLabel(image);
 
-        // Add the label to the frame
         frame.add(label, BorderLayout.CENTER);
 
-        // Display the frame
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
